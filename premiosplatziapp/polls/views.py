1f451b8ed0c09b721f840b6e76bc0ee08b2a39d9
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2022-03-12 15:46:07 -0600

    working with views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse("si jala")
+    return HttpResponse("Estas en la pagina principal nene")
+
+
+def detail(request, question_id):
+    return HttpResponse(f'es la pregunta No. {question_id}')
+
+
+def results(request, question_id):
+    return HttpResponse(f'estos son los resultados de la pregunta No. {question_id}')
+
+
+def vote(request, question_id):
+    return HttpResponse(f'estas votando por la pregunta No. {question_id}')
