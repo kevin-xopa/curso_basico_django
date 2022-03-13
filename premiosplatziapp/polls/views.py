0086318500dfda6d0cb5c757d4c61d1aa388add5
Kevin Ochoa Xopa <kevinochoaxopa@gmail.com>
2022-03-13 01:21:22 -0600

    working with Generic Views

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,23 +2,23 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
-
+from django.views import generic
 # Create your views here.
 
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
-    # return HttpResponse("Estas en la pagina principal nene")
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {"latest_question_list": latest_question_list})
+#     # return HttpResponse("Estas en la pagina principal nene")
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    return HttpResponse(f'estos son los resultados de la pregunta No. {question_id}')
+# def results(request, question_id):
+#     return HttpResponse(f'estos son los resultados de la pregunta No. {question_id}')
 
 
 def vote(request, question_id):
@@ -34,3 +34,19 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """return the last five published questions"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
